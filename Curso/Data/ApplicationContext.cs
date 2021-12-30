35b96451585c0b60396a8270c71b2d3c3b01cd4a
Daniel <freitasdaniel39@gmail.com>
2021-12-30 11:05:23 -0300

    implementando novos metodos de acesso ao banco

--- Curso/Data/ApplicationContext.cs
+++ Curso/Data/ApplicationContext.cs
@@ -1,12 +1,21 @@
+using System;
+using System.Linq;
 using Curso.Domain;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
 
 namespace Curso.Data
 {
     public class ApplicationContext : DbContext
     {
+        private static readonly ILoggerFactory _logger = LoggerFactory.Create(p => p.AddConsole());
+
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder) {
-            optionsBuilder.UseSqlServer("Data source=(localdb)\\mssqllocaldb; Initial Catalog=CursoEFCore;Integrated Security=true");
+            optionsBuilder
+                .UseLoggerFactory(_logger)
+                .EnableSensitiveDataLogging()
+                .UseSqlServer("Data source=(localdb)\\mssqllocaldb; Initial Catalog=CursoEFCore;Integrated Security=true",
+                    p=>p.EnableRetryOnFailure(maxRetryCount: 2));
         }
 
         public DbSet<Pedido> Pedidos { get; set; }
@@ -15,6 +24,24 @@ namespace Curso.Data
 
         protected override void OnModelCreating(ModelBuilder modelBuilder) {
             modelBuilder.ApplyConfigurationsFromAssembly(typeof(ApplicationContext).Assembly);
+            MapearPropriedadesEsquecidas(modelBuilder);
+        }
+
+        private void MapearPropriedadesEsquecidas(ModelBuilder modelBuilder)
+        {
+            foreach(var entity in modelBuilder.Model.GetEntityTypes())
+            {
+                var properties = entity.GetProperties().Where(p=>p.ClrType == typeof(string));
+
+                foreach(var property in properties)
+                {
+                    if(string.IsNullOrEmpty(property.GetColumnType())
+                        && !property.GetMaxLength().HasValue)
+                        {
+                            property.SetColumnType("VARCHAR(100)");
+                        }
+                }
+            }
         }
     }
 }
